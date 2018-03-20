188eaa2486fde98f379cb2ac47b32bf99be27732
huxf <huxfus@gmail.com>
2018-03-20 23:07:49 +0800

    no message

---
+++ MahjongServer/include/socket.h
@@ -0,0 +1,14 @@
+//
+// Created by Ziv on 2018/3/20.
+//
+
+#ifndef MAHJONGSERVER_SOCKET_H
+#define MAHJONGSERVER_SOCKET_H
+
+
+class socket {
+
+};
+
+
+#endif //MAHJONGSERVER_SOCKET_H
---
+++ MahjongServer/src/socket_posix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ziv on 2018/3/20.
+//
+
+#include "socket.h"
