daf9393afbcc9d7cd37ba5ae5b03597725766a54
huxf <huxfus@gmail.com>
2018-03-21 22:32:01 +0800

    no message

--- MahjongServer/src/main.cpp
+++ MahjongServer/src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <thread>
 #include "socket.h"
 
 using namespace std;
 
 int main() {
+
+    thread t([](){
+        cout << "11111" << endl;
+    });
+//    t.join();
+
     Socket socket;
     socket.listen(9999);
     cout << "Hello, World!" << endl;
