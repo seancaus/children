65926c8c99857f86cdb6fed29e401a25512e754b
huxf <huxfus@gmail.com>
2018-05-28 00:46:05 +0800

    no message

--- web/Bridge/config/routes.rb
+++ web/Bridge/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	get 'welcome/index'
 
 	get 'login', to: 'login#index'
+	get 'login/register', to: 'login#register'
 	# resources :login
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
